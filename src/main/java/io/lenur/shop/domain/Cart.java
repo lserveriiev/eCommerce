12dd62afa81cab1e4e920eb48ecaaf9ab12d472d
lenur <lenur.serveriev@gmail.com>
2020-05-09 10:04:01 +0300

    added domain model

---
+++ src/main/java/io/lenur/shop/domain/Cart.java
@@ -0,0 +1,41 @@
+package io.lenur.shop.domain;
+
+import java.util.List;
+
+public class Cart {
+    private Long id;
+    private List<Product> products;
+    private User user;
+
+    public Cart() {
+    }
+
+    public Cart(List<Product> products, User user) {
+        this.products = products;
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
